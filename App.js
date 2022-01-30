6530fd1fa8753183c11e84c85bba1ad421bd6e35
jcamiloguz <jcamiloguz1@gmail.com>
2022-01-30 18:14:56 -0500

    starter expo app: eslint & prettier config

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
   return (
@@ -7,7 +8,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +18,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
